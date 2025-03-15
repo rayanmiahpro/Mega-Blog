433222dbb4adf01933d2f63fdee06ab651031ac2
Rayan Miah <rayanmiahpro@gmail.com>
2025-03-15 20:00:49 +0600

    add same components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,38 @@
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Header, Footer } from "./components";
+import authServises from "./appwrite/Auth";
+import { login, logout } from "./store/authSlice";
+import { Outlet } from "react-router-dom";
+
 function App() {
-  return (
-    <>
-      <h1 className="bg-amber-500 text-3xl text-center">Blog app</h1>
-    </>
-  );
+  const [loading, setLooading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authServises
+      .getCorentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLooading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>
+          TODO: <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import Continer from "./continer/Continer";
+
+export { Header, Footer, Logo, LogoutBtn, Continer };
